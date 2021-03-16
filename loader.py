b5bfb608688c56a89462ae0e000ad8eb8cd24b60
Kseniia <78791737+kseniia777@users.noreply.github.com>
2021-03-16 13:08:32 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -6,7 +6,6 @@ import config
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 admin_id = config.admin_id
 group_id = config.group_id
-present = config.present
 time_stamp = config.time_stamp
 bot_name = config.bot_name
 
