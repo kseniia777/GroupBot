24f03125d750d1a133641d65b6c15de13c26040c
Kseniia <78791737+kseniia777@users.noreply.github.com>
2021-03-16 13:08:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-BOT_TOKEN = '1612583912:AAE0hgk-mKbJle_vO7e_wwYu-p8hatLonx0'
-#BOT_TOKEN = '1538336600:AAFy6VuzXXC1R9eLo2xLz9QWpPRZk9V509Q'
-admin_id = [933693522, 296398759]
-#group_id = -549695150
-group_id = "@testichek67"
+BOT_TOKEN = '***'
+admin_id = [***, ***]
+group_id = "@***"
 time_stamp = 5 #как часто (в часах) присылать сообщение в чат
-present = "Экскурсию ..."
-bot_name = "@ChatJoinMembers_bot"
+bot_name = "@***"
